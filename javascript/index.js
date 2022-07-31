8ccba46f1032f711074eb444acc5e1962e507347
NgocManhLe <ngocmanh26197@gmail.com>
2022-07-31 13:54:39 +0700

    sua lai gia tien

--- javascript/index.js
+++ javascript/index.js
@@ -4,7 +4,7 @@ var listFood = [
         image: '../img/product_1.png',
         title: 'Nem phượng hoàng',
         description: 'Là đặc sản nổi tiếng của Hà Nội, với hương vị hết sức là thân quen!',
-        money: '350.000 vnđ',
+        money: '350000',
         fullstar: '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>',
         halfstar: '<i class="fa-solid fa-star-half-stroke"></i>',
         id: '1'
@@ -13,7 +13,7 @@ var listFood = [
         image: '../img/product_2.png',
         title: 'Trứng vàng kin lâu',
         description: 'Một loại trứng đáng rất được giới tài phiệt săn đón cho mỗi buổi sáng',
-        money: '550.000 vnđ',
+        money: '550000',
         fullstar: '<i class="fa-solid fa-star"></i>  <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>',
         halfstar: '<i class="fa-solid fa-star-half-stroke"></i>',
         id: '2'
@@ -22,7 +22,7 @@ var listFood = [
         image: '../img/product_3.png',
         title: 'Salad tây âu',
         description: 'Là món ăn nhanh rất được ưa chuộng vào mỗi buổi sáng tại châu âu',
-        money: '290.000 vnđ',
+        money: '290000',
         fullstar: '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>',
         id: '3'
     },
@@ -30,7 +30,7 @@ var listFood = [
         image: '../img/product_4.png',
         title: 'Humberger hoàng gia',
         description: 'là thức ăn nhanh có từ những năm thế chiến thứ 2 còn đươc con người lưu giữ tới ngày nay ',
-        money: '999.000 vnđ',
+        money: '999000',
         fullstar: '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>',
         halfstar: '<i class="fa-solid fa-star-half-stroke"></i>',
         id: '4'
@@ -39,7 +39,7 @@ var listFood = [
         image: '../img/product_5.png',
         title: 'Món ăn mắc ca ca',
         description: 'Cực phẩn trong làng ẩm thực trung hoa, hội tụ đầy đủ hương vị dân tộc',
-        money: '99.000 vnđ',
+        money: '99000',
         fullstar: '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>',
         halfstar: '<i class="fa-solid fa-star-half-stroke"></i>',
         id: '5'
@@ -48,7 +48,7 @@ var listFood = [
         image: '../img/product_6.png',
         title: 'Cá sốt ',
         description: 'Yêu ngay từ cái nhìn đầu tiên',
-        money: '650.000 vnđ',
+        money: '650000',
         fullstar: '<i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i> <i class="fa-solid fa-star"></i>',
         halfstar: '<i class="fa-solid fa-star-half-stroke"></i>',
         id: '6'
@@ -103,7 +103,7 @@ var a = listFood.map(food => {
                     <h2 class="manh__food-title">${food.title}</h2>
                     <p class="manh__food-description">${food.description}</p>
                     <div class="manh__food-cost">
-                        <p class="manh__food-money">${food.money}</p>
+                        <p class="manh__food-money">${food.money} VND</p>
                         <i onclick = "addToCart('${food.title}','${food.money}')" 
                         class="fa-solid fa-cart-shopping"></i>
                     </div>
